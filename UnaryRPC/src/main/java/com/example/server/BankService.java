130ea4b9355350e7249166017bb95cc500765a6d
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-12-29 00:56:12 +0300

    first

---
+++ UnaryRPC/src/main/java/com/example/server/BankService.java
@@ -0,0 +1,22 @@
+package com.example.server;
+
+import com.example.models.Balance;
+import com.example.models.BalanceCheckRequest;
+import com.example.models.BankServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class BankService extends BankServiceGrpc.BankServiceImplBase {
+
+    @Override
+    public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
+
+        int accountNumber = request.getAccountNumber();
+        Balance balance = Balance.newBuilder()
+                .setAmount(accountNumber * 50)
+                .build();
+
+        responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+
+    }
+}
---
+++ UnaryRPC/src/test/java/com/example/client/BankClientTest.java
@@ -0,0 +1,35 @@
+package com.example.client;
+
+import com.example.models.Balance;
+import com.example.models.BalanceCheckRequest;
+import com.example.models.BankServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BankClientTest {
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setup() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    @Test
+    public void balanceTest() {
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(7)
+                .build();
+        Balance balance = this.blockingStub.getBalance(balanceCheckRequest);
+        System.out.println("Received : " + balance.getAmount());
+
+    }
+
+}
