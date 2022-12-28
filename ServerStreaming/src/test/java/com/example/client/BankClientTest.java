3dd7374e64aec38e22bfd5ae9bcb143651109b7b
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-12-29 02:21:14 +0300

    async refactor

--- ServerStreaming/src/test/java/com/example/client/BankClientTest.java
+++ ServerStreaming/src/test/java/com/example/client/BankClientTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -37,11 +38,11 @@ public class BankClientTest {
     }
 
     @Test
-    public void withdrawAsyncTest() {
+    public void withdrawAsyncTest() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
         WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder().setAccountNumber(9).setAmount(40).build();
-        this.bankServiceStub.withdraw(withdrawRequest, new MoneyStreamingResponse());
-        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
-
+        this.bankServiceStub.withdraw(withdrawRequest, new MoneyStreamingResponse(latch));
+        latch.await();
 
     }
 
--- ServerStreaming/src/test/java/com/example/client/MoneyStreamingResponse.java
+++ ServerStreaming/src/test/java/com/example/client/MoneyStreamingResponse.java
@@ -3,7 +3,16 @@ package com.example.client;
 import com.example.models.Money;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.CountDownLatch;
+
 public class MoneyStreamingResponse implements StreamObserver<Money> {
+
+    private CountDownLatch latch;
+
+    public MoneyStreamingResponse(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
     @Override
     public void onNext(Money money) {
         System.out.println("Received async: " + money.getValue() + " , Remaining async: " + money.getRemainingValue());
@@ -12,10 +21,12 @@ public class MoneyStreamingResponse implements StreamObserver<Money> {
     @Override
     public void onError(Throwable throwable) {
         System.out.println(throwable.getMessage());
+        latch.countDown();
     }
 
     @Override
     public void onCompleted() {
         System.out.println("Server is Done!");
+        latch.countDown();
     }
 }
