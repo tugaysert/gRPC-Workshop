5f90dac2550be291be5839780db70d7d80730925
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-12-29 02:06:33 +0300

    mock client

---
+++ ServerStreaming/src/test/java/com/example/client/BankClientTest.java
@@ -0,0 +1,34 @@
+package com.example.client;
+
+import com.example.models.Balance;
+import com.example.models.BalanceCheckRequest;
+import com.example.models.BankServiceGrpc;
+import com.example.models.WithdrawRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BankClientTest {
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setup() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    @Test
+    public void withdrawTest() {
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder().setAccountNumber(7).setAmount(40).build();
+        this.blockingStub.withdraw(withdrawRequest)
+                .forEachRemaining(money -> System.out.println("Received: " + money.getValue() + " , Remaining: " + money.getRemainingValue()));
+    }
+
+
+}
