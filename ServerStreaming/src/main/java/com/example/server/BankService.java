087ed57819ec24acf0bbc75efc27bb7f3a8f3372
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-12-29 02:01:45 +0300

    validations, error code

--- ServerStreaming/src/main/java/com/example/server/BankService.java
+++ ServerStreaming/src/main/java/com/example/server/BankService.java
@@ -1,6 +1,7 @@
 package com.example.server;
 
 import com.example.models.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -12,15 +13,17 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
         int amount = request.getAmount();// like 10, 20, 30...
         int balance = AccountDatabase.getBalance(accountNumber);
 
+        if(balance < amount){
+            Status status = Status.FAILED_PRECONDITION.withDescription("No enough money, You have only: " + balance);
+            responseObserver.onError(status.asRuntimeException());
+            return;
+
+        }
+
         for (int i = 0; i < amount/10; i++) {
             AccountDatabase.deductBalance(accountNumber, 10);
             Money money = Money.newBuilder().setValue(10).setRemainingValue(AccountDatabase.getBalance(accountNumber)).build();
             responseObserver.onNext(money);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         }
         responseObserver.onCompleted();
     }
